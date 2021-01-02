a2033bfec47f1f8303b62ab3ac247e9f5cabb0b7
Ujjwal Negi <negiujjawal@gmail.com>
2021-01-02 19:48:37 +0530

    initial commit

---
+++ codeforces.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int n;
+
+void solve() {
+    int x, y; cin >> x >> y;
+    cout << x-1 << " " << y << "\n";
+}
+
+signed main() {
+    fast int t = 1;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
